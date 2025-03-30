8269805e40c79bba483887ed20bd596f8c8a1b17
Hariprasad <hariragul2006@gmail.com>
2025-03-30 13:23:30 +0530

    Added the url of frontend to test

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const sample = require("./routes/sample");
 const auth = require("./routes/auth");
 const clientHistory = require("./routes/clientHistory");
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        methods: "GET, POST, PUT, DELETE",
+        credentials: true
+    })
+);
 app.use(express.json());
 
 app.use(sample);
